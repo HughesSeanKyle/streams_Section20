4d2d47f6be6faf26eca92308045660ba3e70091c
HughesSeanKyle <khughessean@yahoo.com>
2021-01-21 13:52:00 +0200

    V325 - Connecting Redux Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,16 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; // 1.1
 import authReducer from './authReducer';
 
 // Dummy code for redux setup
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
+
+/*
+// 1.1
+When importing a NAMED IMPORT and renaming methods/funcions etc from a module you can use the 'as' keyword in the import statement. 
+
+In addition formReducer must be assigned to a a key named 'form'. if not  done redux will throw an error. 
+*/
