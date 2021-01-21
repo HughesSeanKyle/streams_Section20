e1521d6dd648415821b04c995b9a01c8fb123d0e
HughesSeanKyle <khughessean@yahoo.com>
2021-01-21 13:23:12 +0200

    V320 - Using Redux Dev Tools to Inspect the Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhanchers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhanchers(applyMiddleware())); // 1.1
 
 ReactDOM.render(
     <Provider store={store}>
@@ -14,3 +15,16 @@ ReactDOM.render(
     </Provider>,
     document.querySelector('#root')
 );
+
+
+
+
+
+
+
+
+
+/*
+// 1.1
+Thunk or any other middleware will be added here. 
+*/
