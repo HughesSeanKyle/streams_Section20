6ef91086cfed57155f4c3f6137e6e202f2574145
HughesSeanKyle <khughessean@yahoo.com>
2021-01-22 12:49:07 +0200

    V327 - Automatically Handling Events

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Field, reduxForm } from 'redux-form'; // 1.1
 
 class StreamCreate extends React.Component {
+    renderInput(formProps) {
+        return <input {...formProps.input} /> //1.4
+    }
+
     render() { // 1.3 
         return(
             <form>
-                <Field name="title" />
-                <Field name="description" />
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
             </form>
         ); 
     }
@@ -26,4 +30,7 @@ reduxForm will call a function and we immediately call the function with StreamC
 
 // 1.3
 <Field /> - Is a component that that will solicit any form of input from the user.  
+
+// 1.4 
+Will take all key value pairs and add them as properties to the input element
 */
