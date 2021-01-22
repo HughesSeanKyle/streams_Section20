07486fd0c4b596041872ae7abc3ddbb6fcb015d8
HughesSeanKyle <khughessean@yahoo.com>
2021-01-22 14:08:26 +0200

    V331 - Displaying Validation Messages

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Field, reduxForm } from 'redux-form'; // 1.1
 
 class StreamCreate extends React.Component {
-    renderInput({ input, label }) { //1.4
+    renderInput({ input, label, meta }) { //1.4
         return (
             <div className="field">
                 <label>{label}</label>
                 <input {...input} /> 
+                <div>{meta.error}</div>
             </div>
         ); 
     }
@@ -37,11 +38,12 @@ const validate = (formValues) => { // 1.5
         errors.description = "You must enter a description";
     }
 
-    return errors;
+    return errors; // 1.7
 } 
 
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate  // 1.6
 })(StreamCreate); // 1.2
 
 /*
@@ -60,4 +62,16 @@ reduxForm will call a function and we immediately call the function with StreamC
 
 // 1.5
 At this point the validate function must be hooked/wired up to redux-form
+
+// 1.6
+Can be condensed
+
+// 1.7
+The below can be seen as the big connection between the error object in validate and the field attibute 'name' in the redux-form field component. 
+
+If the error object has an identical name as the name provided within the field component's name attribute and it contains a string that error message will be passed down to the 'this.renderInput' function. 
+
+// 1.8
+renderInput({ input, label, meta }) { //1.4
+    The destructured value 'meta' in this case holds all meta data pertaining to each field componenent. The meta data included the error that was passed through from the validate function. 
 */
