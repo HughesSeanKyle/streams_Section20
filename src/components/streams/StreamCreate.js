d4384f5705c5843710ab21dfcb2f7b3b86e5d430
HughesSeanKyle <khughessean@yahoo.com>
2021-01-22 12:33:36 +0200

    V326 - Creating Forms

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Field, reduxForm } from 'redux-form'; // 1.1
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+    render() { // 1.3 
+        return(
+            <form>
+                <Field name="title" />
+                <Field name="description" />
+            </form>
+        ); 
+    }
+}
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate); // 1.2
+
+/*
+// 1.1
+Field is a component
+reduxForm is a function - Has the same functionality as the connect function form the react-redux library. 
+
+// 1.2
+reduxForm will call a function and we immediately call the function with StreamCreate. 
+
+// 1.3
+<Field /> - Is a component that that will solicit any form of input from the user.  
+*/
